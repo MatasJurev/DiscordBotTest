cfa1e540244185b4f3d520ea3a276b8fcbdb9644
Egils Lapins <egilslapins@gmail.com>
2021-09-16 14:14:23 +0300

    Merged branches

--- src/main/java/discord/bot/Listener.java
+++ src/main/java/discord/bot/Listener.java
@@ -6,6 +6,9 @@ import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+import utilities.CryptoDataParser;
+import utilities.StringUtils;
+import utilities.WebscrapingUtils;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 import yahoofinance.histquotes.HistoricalQuote;
@@ -20,6 +23,8 @@ import java.math.RoundingMode;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.List;
+
+import static utilities.StringUtils.convertDate;
 
 public class Listener extends ListenerAdapter {
 
@@ -78,192 +83,165 @@ public class Listener extends ListenerAdapter {
             channel.sendMessage(test.toString()).queue();
         }
 
-
-       ///FOR STOCKS/CRYPTO, use $ XXX
         try {
             if (msg.startsWith("$") && !bot) {
                 msg = msg.substring(1).toLowerCase();
                 EmbedBuilder eb = new EmbedBuilder();
-                msg = msg.split("\\s+")[1];
-
-                Stock stock = YahooFinance.get(msg.toUpperCase());
-                String titleString =  stock.getName();
-                String symbolString =  stock.getSymbol();
-                eb.setTitle("The Exchange rate of: " + titleString +"("+symbolString+")"+" is: " + (stock.getQuote(true).getPrice()));
                 eb.setColor(Color.red);
-                channel.sendMessage(eb.build()).queue();
-            }
 
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
-        }
+                 if (msg.startsWith("forex")) {
+                    String[] splitted = msg.split("\\s+",2);
+                    msg = splitted[1];
+                    FxQuote currency = YahooFinance.getFx(msg.toUpperCase() + "=X");
 
-        /// GET HISTORY STOCKS/CRYPTO COMMAND
-        try {
-            if (msg.startsWith("History") && !bot) {
-                msg = msg.substring(1).toLowerCase();
-                EmbedBuilder eb = new EmbedBuilder();
-                msg = msg.split("\\s+")[1];
-                Stock stock = YahooFinance.get(msg.toUpperCase());
+                    String title  = currency.getSymbol();
 
-                StringBuilder footer = new StringBuilder();
-                List<HistoricalQuote> history=stock.getHistory();
-                footer.append(System.lineSeparator());
-                footer.append("DATE------PRICE------LOW------HIGH");
-                footer.append(System.lineSeparator());
-                for(HistoricalQuote quote:history) {
-                    footer.append(convertDate(quote.getDate()));
-                    footer.append("  |  " + quote.getClose().setScale(2, RoundingMode.HALF_UP));
-                    footer.append("  |  " + quote.getLow().setScale(2, RoundingMode.HALF_UP));
-                    footer.append("  |  " + quote.getHigh().setScale(2, RoundingMode.HALF_UP));
-                    footer.append(System.lineSeparator());
+                    eb.setTitle(title.substring(0, 6) + " exchange rate:");
+                    eb.setFooter(String.valueOf("Price: " + currency.getPrice()));
+                    channel.sendMessage(eb.build()).queue();
+                }
+                else if (msg.startsWith("stock")) {
+                    msg = msg.split("\\s+")[1];
+                    Stock stock = YahooFinance.get(msg.toUpperCase());
+
+                    eb.setTitle(stock.getName()+" ("+stock.getSymbol()+") " +  "exchange rate:");
+                    eb.setFooter(StringUtils.stockAsString(stock));
+                    channel.sendMessage(eb.build()).queue();
+                }
+                else if (msg.startsWith("crypto")) {
+                    msg = msg.split("\\s+")[1];
+                    Stock stock = YahooFinance.get(msg.toUpperCase() + "-USD");
+
+                    eb.setTitle(stock.getName()+ " exchange rate:");
+                    eb.setFooter(StringUtils.cryptoAsString(stock));
+                    channel.sendMessage(eb.build()).queue();
+                }
+                else if(msg.startsWith("top stocks")) {
+                    String[] symbols = WebscrapingUtils.getTopStocks();
+                    Collection<Stock> stocks = YahooFinance.get(symbols).values();
+                    StringBuilder sb = new StringBuilder();
+
+                    for(Stock stock : stocks) {
+                        if(stock.getName() != null) {
+                            sb.append(StringUtils.stockAsStringWithName(stock));
+                            sb.append("\n");
+                        }
+                    }
+
+                    eb.setTitle("Top 10 stocks:");
+                    eb.setFooter(sb.toString());
+                    channel.sendMessage(eb.build()).queue();
+                }
+                else if(msg.startsWith("top cryptos")) {
+                    StringBuilder sb = new StringBuilder();
+                    String[] symbols = WebscrapingUtils.getTopCryptos();
+                    Collection<Stock> cryptos = YahooFinance.get(symbols).values();
+                    cryptos = CryptoDataParser.getTop10(cryptos);
+
+                    for(Stock crypto : cryptos) {
+                        sb.append(StringUtils.cryptoAsStringWithName(crypto));
+                        sb.append("\n");
+                    }
+
+                    eb.setTitle("Top 10 Cryptocurrencies:");
+                    eb.setFooter(sb.toString());
+                    channel.sendMessage(eb.build()).queue();
+                }
+                /////////////////////////////
+                try {
+                    if (msg.startsWith("history") && !bot) {
+                        msg = msg.split("\\s+")[1];
+                        Stock stock = YahooFinance.get(msg.toUpperCase());
+
+                        StringBuilder footer = new StringBuilder();
+                        List<HistoricalQuote> history=stock.getHistory();
+                        footer.append(System.lineSeparator());
+                        footer.append("DATE------PRICE------LOW------HIGH");
+                        footer.append(System.lineSeparator());
+                        for(HistoricalQuote quote:history) {
+                            footer.append(convertDate(quote.getDate()));
+                            footer.append("  |  " + quote.getClose().setScale(2, RoundingMode.HALF_UP));
+                            footer.append("  |  " + quote.getLow().setScale(2, RoundingMode.HALF_UP));
+                            footer.append("  |  " + quote.getHigh().setScale(2, RoundingMode.HALF_UP));
+                            footer.append(System.lineSeparator());
+                        }
+
+                        eb.setTitle("Monthly Price History of " + stock.getName()+" ("+stock.getSymbol()+")");
+                        eb.setFooter(String.valueOf(footer));
+                        channel.sendMessage(eb.build()).queue();
+                    }
+
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
                 }
 
-                eb.setTitle("Monthly Price History of " + stock.getName()+" ("+stock.getSymbol()+")");
-                eb.setFooter(String.valueOf(footer));
-                eb.setColor(Color.red);
-                channel.sendMessage(eb.build()).queue();
-            }
+                try {
+                    if (msg.startsWith("forex") && !bot){
+                        String[] symbols = new String[] {"EURUSD=X", "GBPUSD=X", "USDJPY=X","AUDUSD=X","USDCAD=X"};
+                        Map<String, FxQuote> currencies = YahooFinance.getFx(symbols);
 
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
-        }
-
-        ///FOR CURRENCIES, use ! xxxXXX
-        try {
-            if (msg.startsWith("!") && !bot) {
-                msg = msg.substring(1).toLowerCase();
-                EmbedBuilder eb = new EmbedBuilder();
-                msg = msg.split("\\s+")[1];
-
-                    FxQuote currency = YahooFinance.getFx(msg.toUpperCase() + "=X");
-                    String titleString =  currency.getSymbol();
-                    titleString = titleString.substring(0, titleString.length() - 2);
-                    eb.setTitle("The Exchange rate of: " + titleString + " is: " + currency.getPrice());
-                    eb.setColor(Color.blue);
+                        FxQuote stock1 = currencies.get(symbols[0]);
+                        FxQuote stock2 = currencies.get(symbols[1]);
+                        FxQuote stock3 = currencies.get(symbols[2]);
+                        FxQuote stock4 = currencies.get(symbols[3]);
+                        FxQuote stock5 = currencies.get(symbols[4]);
+                        eb.setTitle("The Top Currency Prices right now: ");
+                        String text =(
+                                (stock1.getSymbol().substring(0, stock1.getSymbol().length() - 2)) + " trades @ " + stock1.getPrice() + System.lineSeparator() +
+                                        (stock2.getSymbol().substring(0, stock2.getSymbol().length() - 2)) + " trades @ " + stock2.getPrice() + System.lineSeparator() +
+                                        (stock3.getSymbol().substring(0, stock3.getSymbol().length() - 2)) + " trades @ " + stock3.getPrice() + System.lineSeparator() +
+                                        (stock4.getSymbol().substring(0, stock4.getSymbol().length() - 2)) + " trades @ " + stock4.getPrice() + System.lineSeparator() +
+                                        (stock5.getSymbol().substring(0, stock5.getSymbol().length() - 2)) + " trades @ " + stock5.getPrice()
+                        );
+                        eb.setFooter(text);
+                        eb.setColor(Color.red);
+                        channel.sendMessage(eb.build()).queue();;
+                    }
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+                if (msg.startsWith("options") && !bot) {
+                    eb.setTitle("Possible Options are:");
+                    StringBuilder footer = new StringBuilder();
+                    footer.append("$xxx ...for Stocks/Crypto "+System.lineSeparator());
+                    footer.append("!xxx ...for Currencies"+System.lineSeparator());
+                    footer.append("History xxx ...to get monthly price history  "+System.lineSeparator());
+                    footer.append("top stocks ... to get data about top 10 stocks  "+System.lineSeparator());
+                    footer.append("top cryptos ... to get data about top 10 cryptos  "+System.lineSeparator());
+                    eb.setFooter(String.valueOf(footer));
+                    eb.setColor(Color.red);
                     channel.sendMessage(eb.build()).queue();
                 }
 
-            } catch (IOException ioException) {
-            ioException.printStackTrace();
-        }
-        ///////////#Crypto call
-     /*   try {
-        if (msg.startsWith("#Crypto") && !bot){
-            EmbedBuilder eb = new EmbedBuilder();
-            String[] symbols = new String[] {"BTC-USD", "ETH-USD", "ADA-USD","BNB-USD","XRP-USD"};
-            Map<String, Stock> stocks = YahooFinance.get(symbols);
-            Stock stock1 = stocks.get(symbols[0]);
-            Stock stock2 = stocks.get(symbols[1]);
-            Stock stock3 = stocks.get(symbols[2]);
-            Stock stock4 = stocks.get(symbols[3]);
-            Stock stock5 = stocks.get(symbols[4]);
-            eb.setTitle("The Top Cryptocurrency Prices right now: ");
-            String text =
-                    (stock1.getName() + " trades @ " + stock1.getQuote(true).getPrice() + System.lineSeparator() +
-                    stock2.getName() + " trades @ " + stock2.getQuote(true).getPrice() + System.lineSeparator() +
-                    stock3.getName() + " trades @ " + stock3.getQuote(true).getPrice() + System.lineSeparator() +
-                    stock4.getName() + " trades @ " + stock4.getQuote(true).getPrice() + System.lineSeparator() +
-                    stock5.getName() + " trades @ " + stock5.getQuote(true).getPrice());
-            eb.setFooter(text);
-            eb.setColor(Color.red);
-            channel.sendMessage(eb.build()).queue();;
-        }
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
-        }*/
-        ///////////#Stocks call
-    /*    try {
-            if (msg.startsWith("#Stocks") && !bot){
-                EmbedBuilder eb = new EmbedBuilder();
-                String[] symbols = new String[] {"AAPL", "MSFT", "GOOG","AMZN","FB"};
-                Map<String, Stock> stocks = YahooFinance.get(symbols);
-                Stock stock1 = stocks.get(symbols[0]);
-                Stock stock2 = stocks.get(symbols[1]);
-                Stock stock3 = stocks.get(symbols[2]);
-                Stock stock4 = stocks.get(symbols[3]);
-                Stock stock5 = stocks.get(symbols[4]);
-                eb.setTitle("The Top Stock Prices right now: ");
-                String text =
-                        (stock1.getName() + " trades @ " + stock1.getQuote(true).getPrice() + System.lineSeparator() +
-                                stock2.getName() + " trades @ " + stock2.getQuote(true).getPrice() + System.lineSeparator() +
-                                stock3.getName() + " trades @ " + stock3.getQuote(true).getPrice() + System.lineSeparator() +
-                                stock4.getName() + " trades @ " + stock4.getQuote(true).getPrice() + System.lineSeparator() +
-                                stock5.getName() + " trades @ " + stock5.getQuote(true).getPrice());
-                eb.setFooter(text);
-                eb.setColor(Color.red);
-                channel.sendMessage(eb.build()).queue();;
+                if (msg.startsWith("hello") && !bot) {
+                    msg = msg.substring(1).toLowerCase();
+                    StringBuilder footer = new StringBuilder();
+                    footer.append("oooo    oooo    88oooo88"+System.lineSeparator());
+                    footer.append(" 888      888          888    "+System.lineSeparator());
+                    footer.append(" 88888888           888    "+System.lineSeparator());
+                    footer.append(" 888      888          888    "+System.lineSeparator());
+                    footer.append("oooo    oooo    88oooo88"+System.lineSeparator());
+                    eb.setFooter(String.valueOf(footer));
+                    eb.setColor(Color.red);
+                    channel.sendMessage(eb.build()).queue();
+                }
             }
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
-        }*/
-        ///////////#Forex call
-        try {
-            if (msg.startsWith("#Forex") && !bot){
-                EmbedBuilder eb = new EmbedBuilder();
-                String[] symbols = new String[] {"EURUSD=X", "GBPUSD=X", "USDJPY=X","AUDUSD=X","USDCAD=X"};
-                Map<String, FxQuote> currencies = YahooFinance.getFx(symbols);
-
-                FxQuote stock1 = currencies.get(symbols[0]);
-                FxQuote stock2 = currencies.get(symbols[1]);
-                FxQuote stock3 = currencies.get(symbols[2]);
-                FxQuote stock4 = currencies.get(symbols[3]);
-                FxQuote stock5 = currencies.get(symbols[4]);
-                eb.setTitle("The Top Currency Prices right now: ");
-                 String text =(
-                 (stock1.getSymbol().substring(0, stock1.getSymbol().length() - 2)) + " trades @ " + stock1.getPrice() + System.lineSeparator() +
-                 (stock2.getSymbol().substring(0, stock2.getSymbol().length() - 2)) + " trades @ " + stock2.getPrice() + System.lineSeparator() +
-                 (stock3.getSymbol().substring(0, stock3.getSymbol().length() - 2)) + " trades @ " + stock3.getPrice() + System.lineSeparator() +
-                 (stock4.getSymbol().substring(0, stock4.getSymbol().length() - 2)) + " trades @ " + stock4.getPrice() + System.lineSeparator() +
-                 (stock5.getSymbol().substring(0, stock5.getSymbol().length() - 2)) + " trades @ " + stock5.getPrice()
-                 );
-                eb.setFooter(text);
-                eb.setColor(Color.red);
-                channel.sendMessage(eb.build()).queue();;
-            }
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
         }
 
-        if (msg.startsWith("Options") && !bot) {
-            msg = msg.substring(1).toLowerCase();
-            EmbedBuilder eb = new EmbedBuilder();
-            eb.setTitle("Possible Options are:");
-            StringBuilder footer = new StringBuilder();
-            footer.append("$xxx ...for Stocks/Crypto "+System.lineSeparator());
-            footer.append("!xxx ...for Currencies"+System.lineSeparator());
-            footer.append("History xxx ...to get monthly price history  "+System.lineSeparator());
-            footer.append("top stocks ... to get data about top 10 stocks  "+System.lineSeparator());
-            footer.append("top cryptos ... to get data about top 10 cryptos  "+System.lineSeparator());
-
-            eb.setFooter(String.valueOf(footer));
-            eb.setColor(Color.red);
-            channel.sendMessage(eb.build()).queue();
-        }
-
-        if (msg.startsWith("Hello") && !bot) {
-            msg = msg.substring(1).toLowerCase();
-            EmbedBuilder eb = new EmbedBuilder();
-            //msg = msg.split("\\s+")[1];
-
-            StringBuilder footer = new StringBuilder();
-            footer.append("oooo    oooo    88oooo88"+System.lineSeparator());
-            footer.append(" 888      888          888    "+System.lineSeparator());
-            footer.append(" 88888888           888    "+System.lineSeparator());
-            footer.append(" 888      888          888    "+System.lineSeparator());
-            footer.append("oooo    oooo    88oooo88"+System.lineSeparator());
 
 
-            eb.setFooter(String.valueOf(footer));
-            eb.setColor(Color.red);
-            channel.sendMessage(eb.build()).queue();
-        }
 
-    }
-    private String convertDate(Calendar cal){
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-        String formatDate=format.format(cal.getTime());
-        return formatDate;
+
+
+
+
+
+
+
+
     }
 
 }
--- src/main/java/utilities/StringUtils.java
+++ src/main/java/utilities/StringUtils.java
@@ -3,6 +3,9 @@ package utilities;
 import yahoofinance.Stock;
 import yahoofinance.quotes.stock.StockQuote;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class StringUtils {
 
     public static String stockAsString(Stock stock) {
@@ -12,7 +15,8 @@ public class StringUtils {
         sb.append("Price: ");
         sb.append(quote.getPrice());
         sb.append(" USD");
-        sb.append(", change: ");
+        sb.append(System.lineSeparator());
+        sb.append("Daily change: ");
         sb.append(quote.getChange() + "%");
 
         return sb.toString();
@@ -22,15 +26,13 @@ public class StringUtils {
         StringBuilder sb = new StringBuilder();
         StockQuote quote = stock.getQuote();
 
-        sb.append("Name: ");
-        sb.append(stock.getName());
-        sb.append(" [" + stock.getSymbol() + "]");
-        sb.append(", price: ");
+        sb.append("-"+stock.getName());
+        sb.append(" (" + stock.getSymbol() + ")");
+        sb.append(",   price: ");
         sb.append(quote.getPrice());
         sb.append(" USD");
-        sb.append(", change: ");
+        sb.append(",   Daily change: ");
         sb.append(quote.getChange() + "%");
-
         return sb.toString();
     }
 
@@ -41,11 +43,14 @@ public class StringUtils {
         sb.append("Price: ");
         sb.append(quote.getPrice());
         sb.append(" USD");
-        sb.append(", year low: ");
+        sb.append(System.lineSeparator());
+        sb.append("Year low: ");
         sb.append(quote.getYearLow());
-        sb.append(", year high: ");
+        sb.append(System.lineSeparator());
+        sb.append("Year high: ");
         sb.append(quote.getYearHigh());
-        sb.append(", change: ");
+        sb.append(System.lineSeparator());
+        sb.append("Yearly change: ");
         sb.append(quote.getChange() + "%");
 
         return sb.toString();
@@ -56,14 +61,19 @@ public class StringUtils {
         StringBuilder sb = new StringBuilder();
         StockQuote quote = crypto.getQuote();
 
-        sb.append("Name: ");
-        sb.append(crypto.getName());
-        sb.append(", price: ");
+        sb.append("-"+crypto.getName());
+        sb.append(",   price: ");
         sb.append(quote.getPrice());
         sb.append(" USD");
-        sb.append(", change: ");
+        sb.append(",   Daily change: ");
         sb.append(quote.getChange() + "%");
 
         return sb.toString();
     }
+    public static String convertDate(Calendar cal){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String formatDate=format.format(cal.getTime());
+        return formatDate;
+    }
+
 }
